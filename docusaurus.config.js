3e9ed66e3810303895aa972611457d92f4c08f5f
AbinashSelvaraj <abinash@lcscontrols.in>
2023-05-08 19:06:12 +0530

    Added config js file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,15 +11,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://lcscontrols.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'lcscontrols', // Usually your GitHub org/user name.
+  projectName: 'lcscontrols.github.io', // Usually your repo name.
+  deploymentBranch: 'master', // The branch of your site that GitHub pages will use.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
